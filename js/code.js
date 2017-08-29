9348e5b0b4664a248a37e5ecb088b167ff19eda4
HollyJHuber <holly@chooseradical.com>
2017-08-29 15:34:50 -0400

    add: js function, form function, more styles

--- js/code.js
+++ js/code.js
@@ -1,6 +1,29 @@
 /* for HuberPortfolio Deliverable2 */
 
-function montyPython(Name, Quest, Color){
-    var node=document.getElementById("Quest");
-    console.log()
+function MontyPython(){    
+
+    var name = document.getElementsByName("Name")[0].value;
+    var quest = document.getElementsByName("Quest")[0].value;
+    var color;
+    
+    for(var i=0, j=document.getElementsByName("Color").length; i<j; i++){
+        if(document.getElementsByName("Color")[i].selected = "true"){
+            color = document.getElementsByName("Color")[i].value;
+        }
+    }
+    
+    if(name.length <1){
+        name = "Sir Galahad";
+    }
+    if(quest.length <1){
+        quest = "seek the Holy Grail"
+    }
+    
+    var results = name + " is on a quest to " + quest + "!";
+    
+    document.getElementById("formResults").innerHTML = results;
+    document.getElementById("formResults").style.color = color;
+    document.getElementById("formResults").style.display="block";
+    document.getElementById("video").style.display="block";
+    
 };
